d344054c636b4a0503753750b9efd0806189e598
abubakrAhmad1 <156591919+abubakrAhmad1@users.noreply.github.com>
2025-09-30 21:50:30 +0500

    feat: add authentication and protection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,6 +80,13 @@ REST_FRAMEWORK = {
     # Enable pagination globally
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,  # default page size
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
 }
 
 

b2a96066cf23ac4fb940c1c08134d38ba963a064
abubakrAhmad1 <156591919+abubakrAhmad1@users.noreply.github.com>
2025-10-01 14:03:12 +0500

    feat : add card component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return <>
-  <h1 className="bg-black h-16">hello dear</h1>
+  {/* <h1 className="bg-black h-16">hello dear</h1> */}
+  <Card />
     </>;
 }
 

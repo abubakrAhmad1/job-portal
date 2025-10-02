3e040bc152f18f12789fdf893394cb093a491f2c
abubakrAhmad1 <156591919+abubakrAhmad1@users.noreply.github.com>
2025-10-02 20:26:17 +0500

    feat : configure admin portal

--- backend/jobs/admin.py
+++ backend/jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Job
 
-# Register your models here.
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'company', 'location')  # show these fields in the admin list
+    search_fields = ('title', 'company','location')  # add search option
+    list_filter = ('location',)  # add filter on sidebar
+

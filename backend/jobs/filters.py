709903f3e0f5fa25541aa50334bed7b1806f8b38
abubakrAhmad1 <156591919+abubakrAhmad1@users.noreply.github.com>
2025-10-06 12:48:45 +0500

    feat: update filter class

--- backend/jobs/filters.py
+++ backend/jobs/filters.py
@@ -2,9 +2,13 @@ import django_filters
 from .models import Job
 
 class JobFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+    location = django_filters.CharFilter(field_name='location', lookup_expr='icontains')
+    
     class Meta:
         model = Job
         fields = {
+            'title' : ['icontains'],
             'job_type': ['exact'],           # ?job_type=Full-time
             'location': ['exact', 'icontains'],  # ?location=London (exact or partial)
             'tags': ['icontains'],           # ?tag=Pricing
